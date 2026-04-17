c473c56b3cd6f940d801b0e03d62dbbb84862dbc
OneDev-Harsh <harshkumar34.fin@gmail.com>
2026-04-17 22:15:38 +0530

    leetcode26

---
+++ arrays/leetcode26.java
@@ -0,0 +1,29 @@
+class leetcode26 {
+
+    static int removeDuplicates(int[] nums) {
+        if(nums.length == 0) return 0;
+
+        int k = 1;
+
+        for(int i = 1; i < nums.length; i++) {
+            if(nums[i] != nums[i - 1]) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+
+        return k;
+    }
+
+    static void printArray(int[] nums){
+        for(int i=0; i<nums.length; i++){
+            System.out.print(nums[i]+ " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        int nums[] = {0,0,1,1,1,2,2,3,3,4};
+        System.out.println(removeDuplicates(nums));
+        printArray(nums);
+    }
+}
